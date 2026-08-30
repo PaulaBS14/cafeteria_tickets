a4046fbe86ac95cd2f75b7fc5c159cf12304eab6
PaulaBS14 <Paula.b.santos14@gmail.com>
2026-08-30 15:51:52 +0200

    ticket

---
+++ src/Ticket.java
@@ -0,0 +1,46 @@
+public class Ticket {
+
+    private Cliente cliente;
+    private Camarero camarero;
+    private Producto[] productos;
+    private int contadorProductos;
+
+    public Ticket(Cliente cliente, Camarero camarero) {
+        this.cliente = cliente;
+        this.camarero = camarero;
+        this.productos = new Producto[10];
+        this.contadorProductos = 0;
+    }
+
+    public void agregarProducto(Producto producto) {
+        if (contadorProductos < productos.length) {
+            productos[contadorProductos] = producto;
+            contadorProductos++;
+        } else {
+            System.out.println("No se pueden añadir más productos.");
+        }
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+
+        for (int i = 0; i < contadorProductos; i++) {
+            total = total + productos[i].getPrecio();
+        }
+
+        return total;
+    }
+
+    public void mostrarTicket() {
+        System.out.println("CAFETERÍA JAVA");
+        System.out.println("Cliente: " + cliente.getNombre());
+        System.out.println("Camarero: " + camarero.getNombre());
+        System.out.println("Productos:");
+
+        for (int i = 0; i < contadorProductos; i++) {
+            System.out.println(productos[i].getNombre() + " - " + productos[i].getPrecio() + " €");
+        }
+
+        System.out.println("Total: " + calcularTotal() + " €");
+    }
+}
