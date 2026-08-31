716bf3fe907864f5260b3db289406d274c3de1f1
PaulaBS14 <Paula.b.santos14@gmail.com>
2026-08-31 18:41:17 +0200

    bebida_comida_comentarios_añadidos

--- src/Bebida.java
+++ src/Bebida.java
@@ -1,20 +1,44 @@
+/**
+ * Representa una bebida de la cafetería.
+ * @author Paula
+ * @version 1.0
+ */
 public class Bebida extends Producto {
 
     private String tamanio;
 
+    /**
+     * Crea una bebida con su nombre, precio, categoría y tamaño.
+     * @param nombre nombre de la bebida
+     * @param precio precio de la bebida
+     * @param categoria categoría de la bebida
+     * @param tamanio tamaño de la bebida
+     */
     public Bebida(String nombre, double precio, String categoria, String tamanio) {
         super(nombre, precio, categoria);
         this.tamanio = tamanio;
     }
 
+    /**
+     * Devuelve el tamaño de la bebida.
+     * @return tamaño de la bebida
+     */
     public String getTamanio() {
         return tamanio;
     }
 
+    /**
+     * Modifica el tamaño de la bebida.
+     * @param tamanio nuevo tamaño de la bebida
+     */
     public void setTamanio(String tamanio) {
         this.tamanio = tamanio;
 
     }
+
+    /**
+     * Muestra por pantalla la información de la bebida.
+     */
     @Override
     public void mostrarInformacion() {
         System.out.printf("%s %s - %.2f €%n", getNombre(), tamanio, getPrecio());
--- src/Comida.java
+++ src/Comida.java
@@ -1,20 +1,43 @@
+/**
+ * Representa una comida de la cafetería.
+ * @author Paula
+ * @version 1.0
+ */
 public class Comida extends Producto {
 
     private boolean esCaliente;
 
+    /**
+     * Crea una comida con su nombre, precio, categoría y si es caliente.
+     * @param nombre nombre de la comida
+     * @param precio precio de la comida
+     * @param categoria categoría de la comida
+     * @param esCaliente indica si la comida es caliente
+     */
     public Comida(String nombre, double precio, String categoria, boolean esCaliente) {
         super(nombre, precio, categoria);
         this.esCaliente = esCaliente;
     }
 
+    /**
+     * Indica si la comida es caliente.
+     * @return true si la comida es caliente y false si no lo es
+     */
     public boolean getEsCaliente() {
         return esCaliente;
     }
 
+    /**
+     * Modifica si la comida es caliente.
+     * @param esCaliente indica si la comida es caliente
+     */
     public void setEsCaliente(boolean esCaliente) {
         this.esCaliente = esCaliente;
     }
 
+    /**
+     * Muestra por pantalla la información de la comida.
+     */
     @Override
     public void mostrarInformacion() {
         if (esCaliente) {
